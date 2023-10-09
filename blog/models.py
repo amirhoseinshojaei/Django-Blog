c466be7a035a5db3aa8de3b36ce9a46458ec3b74
amirhoseinshojaei <amirootrpr234@gmail.com>
2023-10-10 01:25:05 +0330

    create Blog models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length= 50)
+    description = models.TextField(max_length= 2550)
+    author = models.ForeignKey(User,on_delete= models.CASCADE)
+    pub_date = models.DateTimeField(auto_now=False,auto_now_add=True)

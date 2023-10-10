c0371d2a56b898664d83a33420b68e4d50a610dd
amirhoseinshojaei <amirootrpr234@gmail.com>
2023-10-10 22:32:07 +0330

    create listview for blogs

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import (ListView,DetailView)
+from .models import Blog
 # Create your views here.
+class BlogList(ListView):
+    queryset = Blog.objects.all()
+    context_object_name = 'object_list'
+    template_name = 'blog/blog-list.html'

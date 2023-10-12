cd0afcf35d8ecc6cc67df088bd61adf718357e7e
amirhoseinshojaei <amirootrpr234@gmail.com>
2023-10-12 21:46:15 +0330

    create urls & template for detailview

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BlogList
+from .views import BlogList,BlogDetail
 #Create your urls here
 urlpatterns =[
-    path('',BlogList.as_view(), name= 'blog-list')
+    path('',BlogList.as_view(), name= 'blog-list'),
+    path('blog/<int:pk>/',BlogDetail.as_view(), name= 'blog-detail'),
 ]

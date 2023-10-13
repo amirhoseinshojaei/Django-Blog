1af5641f6ef247b8e9d6eafcc6fece660d43b230
amirhoseinshojaei <amirootrpr234@gmail.com>
2023-10-13 16:42:28 +0330

    create urls for deletview,updateview

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import BlogList,BlogDetail,BlogCreate
+from .views import BlogList,BlogDetail,BlogCreate,BlogUpdate,BlogDelete
 #Create your urls here
 urlpatterns =[
     path('',BlogList.as_view(), name= 'blog-list'),
     path('blog/<int:pk>/',BlogDetail.as_view(), name= 'blog-detail'),
     path ('blog/create',BlogCreate.as_view(), name= 'blog-create'),
+    path ('blog/<int:pk>/update', BlogUpdate.as_view(), name='blog-update'),
+    path ('blog/<int:pk>/delet',BlogDelete.as_view(), name= 'blog-delet'),
 ]

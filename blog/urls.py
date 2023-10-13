41d00c31d76b5608ba0d9dd5bf82306575d3777c
amirhoseinshojaei <amirootrpr234@gmail.com>
2023-10-13 16:14:30 +0330

    create url for createview

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BlogList,BlogDetail
+from .views import BlogList,BlogDetail,BlogCreate
 #Create your urls here
 urlpatterns =[
     path('',BlogList.as_view(), name= 'blog-list'),
     path('blog/<int:pk>/',BlogDetail.as_view(), name= 'blog-detail'),
+    path ('blog/create',BlogCreate.as_view(), name= 'blog-create'),
 ]

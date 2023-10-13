a2fe8afee6e8b5a21d08cc54dd24b8c39e039ad2
amirhoseinshojaei <amirootrpr234@gmail.com>
2023-10-13 17:01:25 +0330

    create login and logout

--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,5 @@ STATICFILES_DIRS =  [BASE_DIR.joinpath('static')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'blog-list'
+LOGOUT_REDIRECT_URL = 'blog-list'

f381708b20c34faa7fcdc3c5214f8ec563868275
amirhoseinshojaei <amirootrpr234@gmail.com>
2023-10-15 20:57:03 +0330

    create CommentInLine

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Blog,Comment
 # Register your models here.
 class BlogAdmin(admin.ModelAdmin):
+    inlines = ['CommentInLine']
     list_display = ['title','author','pub_date']
     list_filter = ['author']
     search_fields = ['title']
@@ -9,4 +10,8 @@ class BlogAdmin(admin.ModelAdmin):
     date_hierarchy = "pub_date"
 
 admin.site.register(Blog,BlogAdmin)
+
+class CommentInLine(admin.TabularInline):
+    model = Comment
+
 admin.site.register(Comment)

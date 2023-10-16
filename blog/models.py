bceaebb7b26a334e25bf2c2de7eaaa032aeb8e8d
amirhoseinshojaei <amirootrpr234@gmail.com>
2023-10-17 00:27:20 +0330

    add AUTH_USER_MODEL to blog model

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length= 50)
     description = models.TextField(max_length= 2550)
-    author = models.ForeignKey(User,on_delete= models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete= models.CASCADE)
     pub_date = models.DateTimeField(auto_now=False,auto_now_add=True)
     
     def __str__(self):
@@ -14,5 +15,5 @@ class Blog(models.Model):
 class Comment(models.Model):
     blog = models.ForeignKey(Blog,related_name='comments',on_delete=models.CASCADE)
     name = models.CharField(max_length=55)
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     comment = models.TextField(max_length=670)

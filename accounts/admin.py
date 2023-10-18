0d712c6470c95659e293af04e9b72e3bb399fde1
amirhoseinshojaei <amirootrpr234@gmail.com>
2023-10-18 21:43:54 +0330

    register model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import CustomUser
 # Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ["username","email","last_login","age","is_staff"]
+    search_fields = ['username']
+
+admin.site.register(CustomUser,CustomUserAdmin)

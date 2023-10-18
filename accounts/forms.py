ab73a89729bdf187045df40cf495d252d5166c99
amirhoseinshojaei <amirootrpr234@gmail.com>
2023-10-18 22:36:57 +0330

    add email to customusercreationform

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,7 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields+('age',)
+        fields = UserCreationForm.Meta.fields+('age','email')
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
